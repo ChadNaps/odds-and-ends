8096107511e2dd9a2b1e8dc52028a789425f1509
Chad Napper <contact@chadnaps.com>
2022-12-30 19:03:33 -0700

    Delete mount-network-drives.sh

--- scripts/mount-network-drives.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo mount -t nfs 192.168.1.5:/media /media/ReadyNAS
-sudo mount -t nfs 192.168.1.5:/backup /mnt/ReadyNAS-Backup
-# sudo mount /dev/sdb1 /mnt/MyBook-Backup/
-# sudo mount -t cifs -o guest,vers=1.0 //192.168.1.6/NASBackup /mnt/MyBookLive-Backup
